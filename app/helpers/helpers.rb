c48f740d4a0a2df6ed642a87e20bfcb841aa59f9
Uma Manandhar <um_2003_8@hotmail.com>
2020-02-26 21:41:12 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,13 +1,14 @@
 class Helpers
     def self.current_user(session)
-         @user = User.find(session[:user_id])
+        User.find(session[:user_id])
     end
 
     def self.is_logged_in?(session)
-        if @user = session[:user_id]
+      # !!current_user(session)
+        if session[:user_id]
             true
         else 
-            @user != session[:user_id]
+            !session[:user_id]
             false
         end
     end
